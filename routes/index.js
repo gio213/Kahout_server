7e2c2e1018c86f542dd05664f857ba5457aecbf3
Giorgi Patsia <gio.patsia@gmail.com>
2023-08-22 16:58:30 +0200

    added questions route

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,19 @@
-<<<<<<< HEAD
-
-import express from 'express'
-import usersRouter from './users.js'
-import quizzRouter from './quizz.js'
-import roomsRouter from './roomsRoutes/rooms.js'
-
 import express from "express";
 import usersRouter from "./users.js";
 import quizzRouter from "./quizz.js";
 import roomsRouter from "./roomsRoutes/rooms.js";
+import questionRouter from "./question.js";
 import authRouter from "./auth.js";
-import joinRoomRouter from "./roomsRoutes/joinRoom.js"
+import joinRoomRouter from "./roomsRoutes/joinRoom.js";
 
 let router = express.Router();
 
 router.use("/users", usersRouter);
 // enter code to join room as player
-router.use('/join_room', joinRoomRouter)
-router.use('/rooms', roomsRouter)
-router.use('/quizz', quizzRouter)
+router.use("/join_room", joinRoomRouter);
+router.use("/rooms", roomsRouter);
+router.use("/quizz", quizzRouter);
 router.use("/", authRouter);
+router.use("/", questionRouter);
 
 export default router;
