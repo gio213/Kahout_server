c9e4d2ea58d917da65d337185c6513388c0188e6
Giorgi Patsia <gio.patsia@gmail.com>
2023-08-23 15:47:51 +0200

    added new route get answerss by id

--- controllers/questionControllers/getQuestionByID.js
+++ controllers/questionControllers/getQuestionByID.js
@@ -1,9 +1,9 @@
-import db from "../../config/db.js";
+import connection from "../../config/db.js";
 
 const get_question_by_id = async (req, res) => {
   const { id } = req.params;
   const query = `SELECT * FROM Questions WHERE id=${id}`;
-  db.query(query, (err, result) => {
+  connection.query(query, (err, result) => {
     if (err || result.length === 0) {
       res.status(404).json({ message: "Question not found" });
       throw err;
